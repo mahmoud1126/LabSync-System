23079448ab176312e8ebf357a863ee400f8fd1e2
luvimsan <luvimsan@gmail.com>
2026-04-29 18:26:41 +0300

    fix(index): comment unprepared setup and add login instead

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 
-require_once __DIR__ . '/config/Database.php';
-require_once __DIR__ . '/core/Router.php';
-require_once __DIR__ . '/core/Controller.php';
-require_once __DIR__ . '/core/App.php';
+require_once __DIR__ . '/pages/login.php';
 
-$app = new App();
+/* require_once __DIR__ . '/config/Database.php'; */
+/* require_once __DIR__ . '/core/Router.php'; */
+/* require_once __DIR__ . '/core/Controller.php'; */
+/* require_once __DIR__ . '/core/App.php'; */
+/* $app = new App(); */
